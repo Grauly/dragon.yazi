080396122bab1c9824e3c5d49c6945d48cd6cc89
Grauly <nils.dellemann@web.de>
2025-04-19 17:43:59 +0200

    feat: drag support

--- main.lua
+++ main.lua
@@ -1,5 +1,15 @@
 local header_name = "dragon"
 
+local options = {
+    { on = "y", run = "export",     desc = "Drag files away with dragon" },
+    { on = "Y", run = "export_all", desc = "Drag all files at once away with dragon" },
+    { on = "d", run = "drop",       desc = "Drag files here with dragon" },
+}
+
+local quit_options = {
+    { on = "q", run = "quit", desc = "Stop accepting files" }
+}
+
 local notify = function(content, level)
     ya.notify {
         title = header_name,
@@ -18,16 +28,16 @@ local error = function(content)
 end
 
 function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
+    if type(o) == 'table' then
+        local s = '{ '
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then k = '"' .. k .. '"' end
+            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
 end
 
 --shamelessly stolen from https://github.com/yazi-rs/plugins/tree/main/chmod.yazi
@@ -42,8 +52,43 @@ local selected_or_hovered = ya.sync(function()
     return paths
 end)
 
+local get_command = function(command)
+    local optionalNixedModule = require(command)
+    return optionalNixedModule.command or command
+end
+
+local handle_export = function(export_all)
+    local paths = selected_or_hovered()
+    local command = Command(get_command("dragon")):arg("--and-exit")
+    if export_all then
+        local all_command = ""
+        if #paths > 10 then
+            all_command = "--all-compact"
+        else
+            all_command = "--all"
+        end
+        command = command:arg(all_command)
+    end
+    command = command:args(paths)
+    local child, err = command:spawn()
+    if err then
+        error(tostring(err))
+    end
+end
+
+local handle_drop = function()
+end
+
+
 return {
-    entry = function ()
-        --TODO :)
+    entry = function()
+        local action = (options[ya.which { cands = options }] or { run = "invalid" }).run
+        if action == "export" then
+            handle_export(false)
+        elseif action == "export_all" then
+            handle_export(true)
+        elseif action == "drop" then
+            handle_drop()
+        end
     end
 }
